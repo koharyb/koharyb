fc2f424f93ed23f8169863f0d527eb9f7bfd895d
koharyb <koharyboris@gmail.com>
2025-04-30 18:32:26 +0200

    second attempt

--- src/components/Before.jsx
+++ src/components/Before.jsx
@@ -4,6 +4,12 @@ const Before = () => {
   return (
     <div>
       <div className="text-xl text-[#f8f9fa]">Previous work experience</div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
+        accusantium praesentium odio impedit nisi non animi cupiditate ex
+        possimus repudiandae assumenda, natus placeat expedita iure distinctio
+        dignissimos. Voluptate, hic consequatur?
+      </p>
     </div>
   );
 };
