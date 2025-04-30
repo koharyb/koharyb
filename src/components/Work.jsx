9b2708250d34070e60cce2ae84728b13ee1154b6
koharyb <koharyboris@gmail.com>
2025-04-30 12:47:26 +0200

    some changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const Work = () => {
   return (
-    <div>
+    <div className="lg:max-w-[200vh] lg:flex lg:items-center lg:justify-center">
       <div className="flex flex-col m-4 mb-10 animate-slideUp">
         <h2 className="text-4xl text-[#fdf9f9] font-bold underline">
           RECENT WORK:
         </h2>
 
-        <div className="border-b-2 border-[#353232]">
+        <div className="border-b-2 border-[#353232] lg:pb-8">
           <a href="" className="text-[#fdf9f9]">
             www.elsyslevice.sk
           </a>
         </div>
-        <div className="border-b-2 border-[#353232]">
+        <div className="border-b-2 border-[#353232] lg:pb-8">
           <a href="" className="text-[#fdf9f9]">
             www.vinecko.sk
           </a>
         </div>
-        <div className="border-b-2 border-[#353232]">
+        <div className="border-b-2 border-[#353232] lg:pb-8">
           <a href="" className="text-[#fdf9f9]">
             www.lakovnalevice.sk
           </a>
         </div>
-        <div className="border-b-2 border-[#353232]">
+        <div className="border-b-2 border-[#353232] lg:pb-8">
           <a href="" className="text-[#fdf9f9]">
             www.jkcatering.net
           </a>
