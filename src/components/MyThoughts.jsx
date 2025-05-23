5b8901943b294e0c63477cf95994263beba5b600
koharyb <koharyboris@gmail.com>
2025-05-24 01:43:47 +0200

    24.01.2025/weekly update for my blog

--- src/components/MyThoughts.jsx
+++ src/components/MyThoughts.jsx
@@ -65,6 +65,44 @@ const MyThoughts = () => {
       >
         {isExpanded ? "Read less..." : "Read more..."}
       </button>
+
+      <h3 className="text-[#f8f9fa] font-bold text-xl">
+        Just buy a old ThinkPad!
+      </h3>
+      <p
+        className={`text-white ${
+          isExpanded ? "" : "line-clamp-3 md:line-clamp-1"
+        } transition-all duration-300`}
+      >
+        Okay, it’s 1:30 in the morning. I can’t sleep again, and my rickety
+        fingers are hovering over the keyboard with an undeniable need to
+        release another thought into the endless space of the internet. So,
+        let’s head back to the start of this week. It happened again... Another
+        laptop has died in my hands. Another poor silicon soul melted under the
+        unavoidable might of my own greatness... or smallness—depends who you
+        ask. Aaaach, I think this makes it the 11th or 12th laptop I’ve
+        destroyed during my career as a Robot Destroyer. Of course, the problem
+        was that freakin' USB-C port. Never mind, Boris. Just breathe. It’s
+        gonna get better—I promise. Okay, but back to the topic. I needed to
+        finish some work ASAP, so I couldn’t wait for the all-mighty Alien (my
+        main laptop) to return from the tech heavens. I needed a fast, reliable
+        replacement. So, I visited one of our local used hardware online shops.
+        And then it happened. I saw him. The HERO of the last decade. One of the
+        final champions of the light. The all-mighty, robust, gorgeous, and
+        black... ThinkPad T470. Pure working power. The last true hero of
+        upgradability still standing. And just for a ridiculous 140 euros! It
+        only had 8GB of RAM. But nothing a 30-euro upgrade couldn’t solve. :P
+        Now, after a quick RAM swap, my hero is under my fingers, purring with
+        the satisfied sound of 20GB of RAM. And honestly? I’m quite convinced I
+        won’t go back to his younger brother, even when he comes back from the
+        SPA. No, really guys... Everyone should own an old ThinkPad!!!
+      </p>
+      <button
+        onClick={toggleReadMore}
+        className="text-blue-400 mt-2 hover:underline focus:outline-none mb-10"
+      >
+        {isExpanded ? "Read less..." : "Read more..."}
+      </button>
     </div>
   );
 };
